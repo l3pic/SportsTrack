3e0a8549450840719218625d1ddd18ec0e317124
l3pic <leovinc1215@gmail.com>
2023-11-26 12:02:49 +0100

    - Ausgabe von daten test

--- app/models/Soccer.php
+++ app/models/Soccer.php
@@ -18,6 +18,30 @@
 
       $apiUrl = APIBASEURL . $link . '?api_key=' . $apikey;
 
-      return getApiData($apiUrl);
+
+
+      $playerids = json_decode(getApiData($apiUrl))->mappings;
+
+      $players = [];
+      foreach($playerids as $index => $playerid) {
+        if ($index > 10) {
+          break;
+        }
+        $profileapi = $this->api->getAPIByName('soccer_profile');
+        $link = $profileapi->link;
+        $apikey = $profileapi->key;
+
+        $link = str_replace(':language_code', 'de', $link);
+        $link = str_replace(':player_id', $playerid->id, $link);
+        $link = str_replace(':format', '.json', $link);
+
+        $apiUrl = APIBASEURL . $link . '?api_key=' . $apikey;
+
+        $player = getApiData($apiUrl);
+
+        array_push($players, $player);
+      }
+
+      return json_encode($players);
     }
   }
