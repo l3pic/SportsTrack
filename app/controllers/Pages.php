ff9b2fc454209318d356094e611296de0eb37f54
Nico <n-helfer@web.de>
2023-12-03 18:08:34 +0100

    Wettertagesvorschau hinzugefügt

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,99 +1,148 @@
 <?php
-  class Pages extends Controller {
-    public function __construct() {
-      $this->weatherModel = $this->model('Weather');
+
+class Pages extends Controller
+{
+    public function __construct()
+    {
+        $this->weatherModel = $this->model('Weather');
     }
 
     // Default method
-    public function index() {
-      if (isset($_SESSION['location'])) {
-        $lat = $_SESSION['location']['lat'];
-        $lon = $_SESSION['location']['lon'];
+    public function index()
+    {
+        if (isset($_SESSION['location'])) {
+            $lat = $_SESSION['location']['lat'];
+            $lon = $_SESSION['location']['lon'];
+            $weatherdata = $this->weatherModel->getWeatherByLatLon($lat, $lon);
+            $city = $this->weatherModel->getCityByGeo($lat, $lon);
+
+            $data = [
+                'weather' => $weatherdata,
+                'city' => $city,
+                'lat' => $lat,
+                'lon' => $lon,
+            ];
+        } else {
+            $data = [];
+        }
+
+        $this->view('pages/index', $data);
+    }
+
+    public function weather($lat = null, $lon = null)
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST' && $lat == null && $lon == null) {
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            $city = trim($_POST['city']);
+            $city = strtolower($city);
+            $city = ucfirst($city);
+
+            $geo = $this->weatherModel->getGeoByCity($city);
+
+            if (empty($geo)) {
+                $data = [
+                    'error' => 'Stadt nicht gefunden',
+                ];
+
+                $this->view('pages/weather', $data);
+                exit();
+            }
+
+            $lat = $geo[0]->lat;
+            $lon = $geo[0]->lon;
+        }
+
         $weatherdata = $this->weatherModel->getWeatherByLatLon($lat, $lon);
         $city = $this->weatherModel->getCityByGeo($lat, $lon);
 
         $data = [
-          'weather' => $weatherdata,
-          'city' => $city,
-          'lat' => $lat,
-          'lon' => $lon,
+            'weather' => $weatherdata,
+            'city' => $city,
+            'lat' => $lat,
+            'lon' => $lon,
         ];
-      } else {
-        $data = [];
-      }
 
-      $this->view('pages/index', $data);
+        $this->view('pages/weather', $data);
     }
 
-    public function weather($lat = null, $lon = null) {
-      if ($_SERVER['REQUEST_METHOD'] == 'POST' && $lat == null && $lon == null) {
-        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+    public function hourforecast($lat = null, $lon = null)
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST' && $lat == null && $lon == null) {
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
-        $city = trim($_POST['city']);
-        $city = strtolower($city);
-        $city = ucfirst($city);
+            $city = trim($_POST['city']);
+            $city = strtolower($city);
+            $city = ucfirst($city);
 
-        $geo = $this->weatherModel->getGeoByCity($city);
+            $geo = $this->weatherModel->getGeoByCity($city);
 
-        if (empty($geo)) {
-          $data = [
-            'error' => 'Stadt nicht gefunden',
-          ];
+            if (empty($geo)) {
+                $data = [
+                    'error' => 'Stadt nicht gefunden',
+                ];
 
-          $this->view('pages/weather', $data);
-          exit();
+                $this->view('pages/weather', $data);
+                exit();
+            }
+
+            $lat = $geo[0]->lat;
+            $lon = $geo[0]->lon;
         }
 
-        $lat = $geo[0]->lat;
-        $lon = $geo[0]->lon;
-      }
+        $weatherdata = $this->weatherModel->getWeatherByLatLon($lat, $lon);
+        $city = $this->weatherModel->getCityByGeo($lat, $lon);
 
-      $weatherdata = $this->weatherModel->getWeatherByLatLon($lat, $lon);
-      $city = $this->weatherModel->getCityByGeo($lat, $lon);
+        $data = [
+            'weather' => $weatherdata,
+            'city' => $city,
+            'lat' => $lat,
+            'lon' => $lon,
+        ];
 
-      $data = [
-        'weather' => $weatherdata,
-        'city' => $city,
-        'lat' => $lat,
-        'lon' => $lon,
-      ];
-
-      $this->view('pages/weather', $data);
+        $this->view('pages/hourforecast', $data);
     }
 
-    public function hourforecast($lat = null, $lon = null) {
-      if ($_SERVER['REQUEST_METHOD'] == 'POST' && $lat == null && $lon == null) {
-        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+    public function dayforecast($lat = null, $lon = null)
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST' && $lat == null && $lon == null) {
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
-        $city = trim($_POST['city']);
-        $city = strtolower($city);
-        $city = ucfirst($city);
+            $city = trim($_POST['city']);
+            $city = strtolower($city);
+            $city = ucfirst($city);
 
-        $geo = $this->weatherModel->getGeoByCity($city);
+            $geo = $this->weatherModel->getGeoByCity($city);
 
-        if (empty($geo)) {
-          $data = [
-            'error' => 'Stadt nicht gefunden',
-          ];
+            if (empty($geo)) {
+                $data = [
+                    'error' => 'Stadt nicht gefunden',
+                ];
 
-          $this->view('pages/weather', $data);
-          exit();
+                $this->view('pages/weather', $data);
+                exit();
+            }
+
+            $lat = $geo[0]->lat;
+            $lon = $geo[0]->lon;
         }
 
-        $lat = $geo[0]->lat;
-        $lon = $geo[0]->lon;
-      }
+        $weatherdata = $this->weatherModel->getWeatherByLatLon($lat, $lon);
+        $city = $this->weatherModel->getCityByGeo($lat, $lon);
 
-      $weatherdata = $this->weatherModel->getWeatherByLatLon($lat, $lon);
-      $city = $this->weatherModel->getCityByGeo($lat, $lon);
+        foreach ($weatherdata->daily as $dayid => $day) {
+            $dayname = date('w', $day->dt);
+            $weatherdata->daily[$dayid]->dt = $this->germanDayNames[$dayname] . ' ' . date('d.m.Y', $day->dt);
+        }
 
-      $data = [
-        'weather' => $weatherdata,
-        'city' => $city,
-        'lat' => $lat,
-        'lon' => $lon,
-      ];
+        $data = [
+            'weather' => $weatherdata,
+            'city' => $city,
+            'lat' => $lat,
+            'lon' => $lon,
+        ];
 
-      $this->view('pages/hourforecast', $data);
+        $this->view('pages/dayforecast', $data);
     }
-  }
+
+}
---
+++ app/views/pages/dayforecast.php
@@ -0,0 +1,90 @@
+<head>
+  <link rel="stylesheet" href="<?= URLROOT; ?>/css/weather.css">
+</head>
+<?php require_once APPROOT . '/views/inc/header.php'; ?>
+<?php require_once APPROOT . '/views/inc/sidenav.php'; ?>
+<div class="main">
+<?php if (isset($data['weather'])) : ?>
+  <script>console.log(<?= json_encode($data['weather']); ?>)</script>
+
+  <div class="local-weather">
+    <div class="lw-row">
+      <i class="fa-solid fa-location-dot lw-loc"></i>
+      <h1 class="lw-city"><?= $data['city'][0]->name; ?></h1>
+    </div>
+    <div class="lw-row">
+      <img src="https://openweathermap.org/img/wn/<?= $data['weather']->current->weather[0]->icon; ?>@2x.png" alt="<?= $data['weather']->current->weather[0]->description; ?>" class="lw-icon">
+      <span class="lw-temp"><?= $data['weather']->current->temp; ?>°C</span>
+    </div>
+    <div class="lw-row">
+      <span class="lw-desc"><?= $data['weather']->current->weather[0]->description; ?></span>
+      <span class="lw-time"><?= date('H:i', $data['weather']->current->dt); ?></span>
+    </div>
+  </div>
+
+  <form class="search-city" action="<?= URLROOT; ?>/pages/dayforecast" method="POST">
+    <input type="text" name="city" placeholder="Stadt" required>
+    <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+  </form>
+
+  <div class="links-container">
+    <div class="links">
+      <a href="<?= URLROOT; ?>/pages/weather/<?= $data['city'][0]->lat; ?>/<?= $data['city'][0]->lon; ?>"
+         class="link">
+        <i class="fa-solid fa-calendar-day"></i>
+        <span class="link-text">Heute</span>
+      </a>
+    </div>
+    <div class="links">
+      <a href="<?= URLROOT; ?>/pages/hourforecast/<?= $data['city'][0]->lat; ?>/<?= $data['city'][0]->lon; ?>"
+         class="link">
+        <i class="fa-solid fa-clock"></i>
+        <span class="link-text">Stündlich</span>
+      </a>
+    </div>
+    <div class="links aria-inactive">
+      <a href="<?= URLROOT; ?>/pages/dayforecast/<?= $data['city'][0]->lat; ?>/<?= $data['city'][0]->lon; ?>"
+         class="link">
+        <i class="fa-solid fa-calendar-days"></i>
+        <span class="link-text">Täglich</span>
+      </a>
+    </div>
+    <div class="links">
+      <a href="<?= URLROOT; ?>/pages/pollution/<?= $data['city'][0]->lat; ?>/<?= $data['city'][0]->lon; ?>"
+         class="link">
+        <i class="fa-solid fa-smog"></i>
+        <span class="link-text">Luftverschmutzung</span>
+      </a>
+    </div>
+  </div>
+
+  <div class="df-container">
+    <?php foreach ($data['weather']->daily as $day) : ?>
+      <div class="df-row">
+        <div>
+          <span class="df-time"><?= $day->dt; ?></span>
+          <img src="https://openweathermap.org/img/wn/<?= $day->weather[0]->icon; ?>@2x.png"
+               alt="<?= $day->weather[0]->description; ?>" class="df-icon">
+          <span class="df-desc"><?= $day->weather[0]->description; ?></span>
+          <span class="df-temp"><?= $day->temp->min; ?>°C</span>
+          <span class="df-desc">Min.</span>
+          <span class="df-temp"><?= $day->temp->max; ?>°C</span>
+          <span class="df-desc">Max.</span>
+        </div>
+        <div>
+          <span class="df-temp"><?= $day->temp->morn; ?>°C</span>
+          <span class="df-desc">Morgens</span>
+          <span class="df-temp"><?= $day->temp->day; ?>°C</span>
+          <span class="df-desc">Tag</span>
+          <span class="df-temp"><?= $day->temp->eve; ?>°C</span>
+          <span class="df-desc">Nachmittag</span>
+          <span class="df-temp"><?= $day->temp->night; ?>°C</span>
+          <span class="df-desc">Nacht</span>
+        </div>
+      </div>
+    <?php endforeach; ?>
+  </div>
+<?php else : ?>
+  <!--error-->
+<?php endif; ?>
+</div>
