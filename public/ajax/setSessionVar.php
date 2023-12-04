e669abbad7276de937c9c09e19563e33502eb219
l3pic <leovinc1215@gmail.com>
2023-12-04 14:03:47 +0100

    Success überprüfung für session var

--- public/ajax/setSessionVar.php
+++ public/ajax/setSessionVar.php
@@ -4,3 +4,9 @@
   $varValue = $_POST['varValue'];
 
   $_SESSION[$varName] = $varValue;
+
+  if(isset($_SESSION[$varName])) {
+    echo json_encode(array('success' => true));
+  } else {
+    echo json_encode(array('success' => false));
+  }
