4931f0390e8070edaeef8e9132a5cf90f6d5f117
l3pic <leovinc1215@gmail.com>
2023-12-04 17:08:57 +0100

    Sidenav fix hat sich immer von alleine geöffnet

--- public/js/main.js
+++ public/js/main.js
@@ -7,9 +7,10 @@ function togglesidenav(element) {
 }
 
 function openSidebar() {
+    console.log('test')
     sessionStorage.setItem('sidebarOpen', 'true');
     // Weitere Logik, um die Seitenleiste zu öffnen
-    $('#sidenav').toggleClass('open');
+    $('#sidenav').addClass('open');
     $("html").css("--sidenav-width", '250px');
     $('.sidenav-toggle').replaceWith('<svg xmlns="http://www.w3.org/2000/svg" height="1.2em" viewBox="0 0 384 512" style="fill:#ffffff" class="sidenav-toggle" onclick="togglesidenav(this);"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com/ License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>')
 }
@@ -17,7 +18,7 @@ function openSidebar() {
 function closeSidebar() {
     sessionStorage.setItem('sidebarOpen', 'false');
     // Weitere Logik, um die Seitenleiste zu schließen
-    $('#sidenav').toggleClass('open');
+    $('#sidenav').removeClass('open');
     $("html").css("--sidenav-width", '0px');
     $('.sidenav-toggle').replaceWith('<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512" style="fill:#ffffff" class="sidenav-toggle" onClick="togglesidenav(this);"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com/ License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>')
 }
@@ -84,12 +85,14 @@ if (locationIsset == 0) {
 $(document).ready(function () {
     const currentDate = new Date();
     const currentHour = currentDate.getHours();
-    var sidebarOpen = sessionStorage.getItem('sidebarOpen');
-    if (sidebarOpen === 'true') {
+    let sidebarOpen = sessionStorage.getItem('sidebarOpen');
+    if (sidebarOpen == 'true') {
         // Öffne die Seitenleiste
+        console.log('test', sidebarOpen)
         openSidebar()
     } else {
         // Schließe die Seitenleiste
+        console.log('test', sidebarOpen)
         closeSidebar()
     }
 // Zeitintervall
